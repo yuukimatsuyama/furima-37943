eeb5e9d66e67e4dfbfbab9dd4aa532b755efee38
yuukimatsuyama <104548848+yuukimatsuyama@users.noreply.github.com>
2022-05-19 14:34:01 +0900

    basic認証の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
-  def index
+  before_action :basic_auth
+
+
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username,password|
+      username == 'matsur2' && password == '2150'
+    end
   end
+
 end
