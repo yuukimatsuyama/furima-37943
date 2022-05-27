c43ea119407fab9e3e179b716985b5e008e45915
yuukimatsuyama <104548848+yuukimatsuyama@users.noreply.github.com>
2022-05-27 15:01:08 +0900

    商品出品機能の修正
    商品出品機能の再提出

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -76,6 +76,11 @@ RSpec.describe Item,type: :model do
           @item.valid?
           expect(@item.errors.full_messages).to include("Price is not included in the list")
         end
+        it 'userが紐づいてなければ出品できない' do
+          @item.user = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("User must exist")
+        end
       end
     end
 end
